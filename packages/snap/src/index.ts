30f492c7b484f66cf900a210fed8d5faf74aeae6
Elli610 <nathan@cypherlab.fr>
2024-05-17 12:08:19 +0200

    add Private airdrop claim fct

---
+++ packages/snap/src/index.ts
@@ -0,0 +1,54 @@
+import { OnRpcRequestHandler, UnauthorizedError } from '@metamask/snaps-sdk';
+import { exportAccount, getAddresses, getNewAccount, importAccount } from './rpc/account';
+import { LSAG_Signature } from './rpc/LSAG_Signature';
+import { PAC_LSAG_Signature } from './rpc/PAC_LSAG_Signature';
+
+/**
+ * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
+ *
+ * @param args - The request handler args as object.
+ * @param args.origin - The origin of the request, e.g., the website that
+ * invoked the snap.
+ * @param args.request - A validated JSON-RPC request object.
+ * @returns The result of `snap_dialog`.
+ * @throws If the request method is not valid for this snap.
+ */
+export const onRpcRequest: OnRpcRequestHandler = async ({
+  origin,
+  request,
+}) => {
+
+  // reset storage -> fort debug
+  // await snap.request({
+  //   method: 'snap_manageState',
+  //   params: { operation: ManageStateOperation.ClearState },
+  // }) as object as State;
+
+  switch (request.method) {
+    case 'newAccount':
+      return await getNewAccount();
+
+    case 'importAccount':
+      return await importAccount();
+
+    case 'exportAccount':
+      const address = (request.params as { address: string }).address;
+      if (!address) throw new UnauthorizedError('Valid address is required');
+      return await exportAccount(address);
+
+    case 'getAddresses':
+      return await getAddresses();
+
+    case 'LSAG_Signature':
+      const { ring, message, addressToUse, linkabilityFlag } = (request.params as { ring: string[], message: string, addressToUse: string, linkabilityFlag: string });
+      console.log('LSAG_Signature');
+      return await LSAG_Signature(ring, message, addressToUse, linkabilityFlag);
+
+    case 'PrivateAirdropClaim_LSAG_Signature':
+      const payload = (request.params as { ring: string[], claim_contract_address: string, addressToUse: string });
+      return await PAC_LSAG_Signature(payload.ring, payload.claim_contract_address, payload.addressToUse);
+
+    default:
+      throw new Error('Method not found.');
+  }
+};
---
+++ packages/snap/src/rpc/PAC_LSAG_Signature.ts
@@ -0,0 +1,88 @@
+import { Curve, CurveName, Point, RingSignature } from "@cypher-laboratory/alicesring-lsag";
+import { State } from "../interfaces";
+import { DialogType, text, panel, ManageStateOperation, heading, copyable } from "@metamask/snaps-sdk";
+
+
+export async function PAC_LSAG_Signature(ring: string[], claim_contract_address: string, addressToUse: string): Promise<string> {
+  const secp256k1 = new Curve(CurveName.SECP256K1);
+  const deserializedRing = ring.map((point) => Point.deserialize(point));
+
+  // get private key from storage
+  const state: State = await snap.request({
+    method: 'snap_manageState',
+    params: { operation: ManageStateOperation.GetState },
+  }) as object as State;
+
+  if (!state || !state.account) throw new Error('No account found');
+
+  // get the private key from the account. else throw error
+  const privateKey = state.account.find((acc) => acc.address === addressToUse)?.privateKey;
+  // console.log('state:\n', state?.account);
+  // console.log("str\n", JSON.stringify(state));
+  // console.log("test\n", typeof (state.account[0]?.address), state.account[0]?.address);
+  // console.log('privateKey:', privateKey);
+  if (!privateKey) throw new Error('No private key found');
+
+  // get the claimer receiving address:
+  let address: string | undefined = undefined;
+  let previousIsFalse = false;
+
+  do {
+
+    const displayedPanel = previousIsFalse ? [
+      heading('Claimer Address'),
+      text('**The last address you entered is invalid. Please enter a valid address**'),
+      text('Enter the address you will use to claim the reward:'),
+    ] : [
+      heading('Claimer Address'),
+      text('Enter the address you will use to claim the reward:')
+    ]
+
+    address = (await snap.request({
+      method: 'snap_dialog',
+      params: {
+        type: DialogType.Prompt,
+        content: panel(
+          displayedPanel
+        ),
+      },
+    }))?.toString();
+
+    if(address === undefined) throw new Error('User cancelled the lsag signature process');
+
+    if (!address || !/^(0x)?[0-9a-fA-F]{40}$/.test(address as string)) previousIsFalse = true;
+    // check if the address is a valid hex string with 42 characters. if it is not, ask the user to enter a valid address
+  } while (!address || !/^(0x)?[0-9a-fA-F]{40}$/.test(address as string));
+
+
+  console.log('address:', address);
+  const message = JSON.stringify({
+    claimContractAddress: claim_contract_address,
+    claimerAddress: address,
+  });
+  console.log('message:', message);
+  const approval = await snap.request({
+    method: 'snap_dialog',
+    params: {
+      type: DialogType.Confirmation,
+      content: panel([
+        heading(`Ring Sign a message:`),
+        // text('Signature process can take up to 20 seconds. please wait.'),
+        text('Allow this snap to sign these content?'),
+        text('Claim contract address: '),
+        copyable(claim_contract_address),
+        text('Claimer address: '),
+        copyable(address as string),
+        text('Ring size: ' + ring.length),
+        text('Message:'),
+        copyable(message),
+      ]),
+    },
+  });
+  // console.log('approval:', approval);
+  if (!approval) throw new Error('User denied signing message');
+  console.log('enter signing process');
+  const signature = RingSignature.sign(deserializedRing, BigInt(privateKey), message, secp256k1, claim_contract_address);
+  console.log('signature:', signature.toBase64());
+  return JSON.stringify(signature.toBase64());
+}
